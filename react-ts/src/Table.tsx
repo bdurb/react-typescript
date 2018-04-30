921fe09b1bee6ff1a0706593f4accaa0d5b684ff
bdurb <brian@rpmgreenville.com>
2018-04-30 13:20:07 -0700

    experimenting with material-ui

--- react-ts/src/Table.tsx
+++ react-ts/src/Table.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react';
+// import { withStyles } from 'material-ui/styles';
+// import { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
 
 export default class Table extends React.Component  {
+  constructor(props:any) {
+    super(props);
+
+    this.state = {
+      date: Date.now,
+      expense: props.expense,
+      name: props.name
+    };
+  }
   public render() {
     return (
       <div>

c35248b7a0f5cd96a90b6952e797a61a0f62cc8b
bdurb <brian@rpmgreenville.com>
2018-05-02 10:20:05 -0700

    experimenting with material ui and tables

--- react-ts/src/Table.tsx
+++ react-ts/src/Table.tsx
@@ -1,34 +1,55 @@
+import Button from 'material-ui/Button';
+import Input from 'material-ui/Input';
 import * as React from 'react';
-// import { withStyles } from 'material-ui/styles';
-// import { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
 import './Table.css';
 
-export default class Table extends React.Component  {
+
+export default class Table extends React.Component<{}, {expense: string, amount: number, date: number}> {
   constructor(props:any) {
     super(props);
 
     this.state = {
-      date: Date.now,
-      expense: props.expense,
-      name: props.name
+      amount: 0,
+      date: 0,
+      expense: ''
     };
   }
+
+  public onExpenseChange = (e: any) => {
+    const expense: string = e.target.value;
+    this.setState(() => ({ expense }));
+  }
+
+  public onAmountChange = (e: any) => {
+    const amount: number = e.target.value;
+    this.setState(() => ({ amount }));
+  }
+
+
+  public handleSubmit = (e: any) => {
+    this.setState(() => ({
+      amount: this.state.amount,
+      date: this.state.date,
+      expense: this.state.expense
+    }))
+  }
+
   public render() {
     return (
-      <div className="box">
+      <div>
         <table>
           <tr>
-            <th>Company</th>
             <th>Expense</th>
             <th>Amount</th>
             <th>Date</th>
           </tr>
           <tr>
-            <td>ABC</td>
-            <td>Printer</td>
-            <td>$500.00</td>
-            <td>04/21/2018</td>
-            <button>Submit</button>
+            <td><Input type="text" value={this.state.expense} onChange={this.onExpenseChange}/></td>
+            <td><Input type="number" value={this.state.amount} onChange={this.onAmountChange}/></td>
+            <td><Input type="date" value={this.state.date}/></td>
+            <Button size="small" variant="raised" color="primary" onClick={this.handleSubmit}>
+              Submit
+            </Button>
           </tr>
         </table>
       </div>
